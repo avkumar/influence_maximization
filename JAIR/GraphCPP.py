582769e6af1ca7382e33179690c831e7f34fc7a6
aravasai <aravasai@gmail.com>
2014-07-13 21:01:41 +0530

    added a 4 node graph as input

--- JAIR/GraphCPP.py
+++ JAIR/GraphCPP.py
@@ -2,8 +2,12 @@
 
 import exactMethods
 import mcMethods
-from exactMethods import computeExactGame1
+from exactMethods import computeShapExactGame1
 from mcMethods import computeMCGame1
+from exactMethods import computeBanzExactGame1
+from mcMethods import computeMCGame5
+from exactMethods import computeBanzBruteForceGame5
+from exactMethods import computeShapBruteForceGame5
 '''
 
 void readGraph(char *name, bool weighted){
@@ -52,7 +56,23 @@ def readGraph(filename, isWeighted) :
 #    print adj        
     return (numNodes, numEdges, adj)
 
-                         
+
+def readRandomGraph(size):
+    adj = {}
+    for i in range(size):
+        adj[i] = {}
+        for j in range(i + 1, size):
+            if not adj.__contains__(j):
+                adj[j] = {}
+            weight = random.randint(0,5)
+            adj[i][j] = weight 
+            adj[j][i] = weight
+            
+    return adj       
+            
+                        
+
+
 '''
 
 void readRandomGraph(int size){
@@ -71,6 +91,10 @@ void readRandomGraph(int size){
     }
 }
 '''
+def countAvgDist(G1):
+    return 1 
+
+
 '''
 double countAvgDist(){
     double avgDis=0;
@@ -117,20 +141,48 @@ int main(void)
 }
 
 '''
+def getWcutoff (numNodes):
+    Wcutoff = [1]*numNodes
+    return Wcutoff
+
 if __name__ =="__main__":
     maxIteration = 0
-    maxIteration = 12000
+    maxIteration = 12
     G1 = 'power.txt'
     G2 = 'astro-ph.txt'
-    numNodes, numEdges,  adj =  readGraph(G1, True)
-#    print numNodes, numEdges, adj
-    shapExact1 = computeExactGame1(numNodes, adj)
-    shapComputeMCGame1 = computeMCGame1(numNodes, adj, maxIteration, 'shap')
-    banzComputeMCGame1 =   computeMCGame1(numNodes, adj, maxIteration, 'banz')
-
-    
-    
-    print shapExact1
-    print shapComputeMCGame1
-    print "banzComputeMCGAme1"
-    print banzComputeMCGame1
+    G3 = '4node.txt'
+    numNodes, numEdges,  adj =  readGraph(G3, True)
+    print numNodes, numEdges, adj
+#    shapExactGame1 = computeShapExactGame1(numNodes, adj) 
+#    print "shapExactGame1 is ", shapExactGame1 
+#    banzExactGame1 = computeBanzExactGame1(numNodes, adj)
+#    print "banzExactGame1 is", banzExactGame1
+#    shapMCGame1 = computeMCGame1(numNodes, adj, maxIteration, 'shap')
+#    print "shapMCGame1 is", shapMCGame1
+#    banzMCGame1 = computeMCGame1(numNodes, adj, maxIteration, 'banz')
+#    print "banMCGame1 is", banzMCGame1
+    Wcutoff = getWcutoff(numNodes)
+#    shapMCGame5 = computeMCGame5(numNodes, adj, 12, Wcutoff, 'shap')
+#    print "shapMCGame5 is", shapMCGame5
+#    banzMCGame5 = computeMCGame5(numNodes, adj, 3, Wcutoff, 'banz')
+#    print "banzMCGame5 is", banzMCGame5
+#    numThreshhold = [2]*numNodes
+#
+#    shapExactGame2 = computeShapExactGame2(numNodes, adj, numThreshhold) 
+#    print "shapExactGame2 is ", shapExactGame2 
+#    banzExactGame2 = computeBanzExactGame2(numNodes, adj, numThreshhold)
+#    print "banzExactGame2 is", banzExactGame2
+#    shapMCGame2 = computeMCGame2(numNodes, adj, maxIteration, numThreshhold, 'shap')
+#    print "shapMCGame2 is", shapMCGame2
+#    banzMCGame2 = computeMCGame2(numNodes, adj, maxIteration, numThreshhold, 'banz')
+#
+#    shapExactBruteGame5 =  computeShapBruteForceGame5(numNodes, adj, Wcutoff)
+#    print shapExactBruteGame5
+#    banzExactBrute =  computeBanzBruteForceGame5(numNodes, adj, Wcutoff)
+#    banzExactBruteGame5 = computeBanzBruteForceGame5(numNodes, adj, Wcutoff)
+#    print banzExactBruteGame5
+#    shapMCGame5 = computeMCGame5(numNodes, adj, maxIteration, Wcutoff, 'shap')
+#    print shapMCGame5
+    banzMCGame5 = computeMCGame5(numNodes, adj, maxIteration, Wcutoff , 'banz')
+    print banzMCGame5
+#
